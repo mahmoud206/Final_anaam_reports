dbcd9a12f077dfcfa68428c36153826692b541d5
mahmoud206 <mahmoud199000@gmail.com>
2025-04-03 18:08:26 +0300

    done
    updated_final

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,9 +3,7 @@ allprojects {
         google()
         mavenCentral()
     }
-};
-
-
+}
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
@@ -17,13 +15,6 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
 }
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-}
-
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
