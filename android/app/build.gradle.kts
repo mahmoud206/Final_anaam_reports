960dd552f579a84b1526dd51a2f3a427761d41eb
mahmoud206 <mahmoud199000@gmail.com>
2025-04-03 17:47:51 +0300

    erer
    updated gradles

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,44 +1,31 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
 android {
     namespace = "com.example.vetra_anaam_report"
-    compileSdk = 34 // Use only one definition
-
-    ndkVersion = "27.0.12077973"
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
+    compileSdk = 34
+    
     defaultConfig {
         applicationId = "com.example.vetra_anaam_report"
         minSdk = 21
-        targetSdk = 34 // Ensure it matches compileSdk
+        targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-
-            // TODO: Configure a proper release signingConfig
-            // signingConfig = signingConfigs.getByName("release")
+            hrinkResources = false
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
 
-flutter {
-    source = "../.."
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
 }
